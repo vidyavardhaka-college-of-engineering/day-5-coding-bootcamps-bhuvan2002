d1de777120578be58b7fcc3aeb6562f5c4a5fc82
BHUVAN U KADLAS <bhuvanumesh123@gmail.com>
2021-02-19 11:16:41 +0000

    Wrote a program to reverse 'str' and print it.

--- Program_2.c
+++ Program_2.c
@@ -1,14 +1,30 @@
-//You will be given a string 'str'. Your task is to reverse 'str' and print it.
+//You will be given a string 'str'. Your task is 
 //Input: vvce & Output: ecvv
 
 #include<stdio.h>
 
 int main()
 {
-	char str[10];
-	scanf("%s", str);
+  char str[10],rev[10];
+  int count=0, end, begin;
+  scanf("%s", str);
 
-	//Write your code here
+  //Write your code here
+ while (str[count] != '\0')
+      count++;
 
-	return 0;
+   end = count - 1;
+
+   for (begin = 0; begin < count; begin++) 
+   {
+      rev[begin] = str[end];
+      end--;
+   }
+
+   rev[begin] = '\0';
+
+   printf("%s\n", rev);
+
+   return 0;
+
 }
--- Program_3.c
+++ Program_3.c
@@ -6,12 +6,29 @@
 //output:
 //Palindrome
 #include<stdio.h>
-
+#include <string.h>
 int main()
 {
 	char str[10];
+   char string1[20];
+  int i, length;
+    int flag = 0;
 	scanf("%s", str);
-
+    length = strlen(string1);
+    
+    for(i=0;i < length ;i++){
+        if(string1[i] != string1[length-i-1]){
+            flag = 1;
+            break;
+   }
+}
+    
+    if (flag) {
+        printf("%s is not a palindrome", string1);
+    }    
+    else {
+        printf("%s is a palindrome", string1);
+    }
 	//Write your code here
 	
 	return 0;
