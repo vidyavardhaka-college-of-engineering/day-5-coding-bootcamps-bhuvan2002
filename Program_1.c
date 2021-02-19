ba01761e91c2d156b4d595ad14ba1f61180332ce
BHUVAN U KADLAS <bhuvanumesh123@gmail.com>
2021-02-19 11:07:38 +0000

    Wrote a program to find the number of vowels in 'str' and print it

--- Program_1.c
+++ Program_1.c
@@ -5,9 +5,23 @@
 int main()
 {
 	char str[10];
+  int count=0,i;
 	scanf("%s", str);
 
 	//Write your code here
+  for(i=0;str[i]!='\0';i++)
+  {
+    if(str[i]=='a'||
+       str[i]=='e'||
+       str[i]=='i'||
+       str[i]=='o'||
+       str[i]=='u' )
+       {
+         count++;
+       }
+  }
+  printf("%d",count);
+
 
 	return 0;
 }
